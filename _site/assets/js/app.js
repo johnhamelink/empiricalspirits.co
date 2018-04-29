57dd58082da19a204ea9d967210c3f65d2ed976f
Gareth Johns <info@garethjohnsdesign.com>
2018-04-29 23:44:20 +0100

    Initial Commit

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -72,5 +72,6 @@ $('#fullpage').fullpage({
   anchors: ['firstPage', 'secondPage', '3rdPage', '4thpage', 'lastPage'],
 	menu: '#menu',
 	scrollingSpeed: 1000,
-	scrollBar:true
+	scrollBar:true,
+	parallax: true
 });
