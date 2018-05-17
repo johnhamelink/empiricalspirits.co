a380eb00d12a250c2489d93fb4a74cc9f2d48eea
Gareth Johns <info@garethjohnsdesign.com>
2018-05-17 12:28:04 +0100

    Updated Foundation and 5 other files

---
+++ assets/js/foundation.js
@@ -0,0 +1,18 @@
+$(document).foundation();
+
+// 1. Loading
+// --------------------
+
+$(function() {
+if (sessionStorage.getItem('set') === 'set') {
+  $('.loader').hide();
+} else {
+  $('.loader').show();
+/*
+  setTimeout(function(){
+        $('.loader').addClass('loaded');
+    }, 1500); 
+*/ 
+  sessionStorage.setItem('set', 'set');
+}
+});
