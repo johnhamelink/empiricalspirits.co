90fef686b13e2d343c401d33a589d31ea32db2de
Gareth Johns <info@garethjohnsdesign.com>
2018-05-17 12:00:53 +0100

    Updated Default and 2 other files

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,9 +8,11 @@ if (sessionStorage.getItem('set') === 'set') {
   $('.loader').hide();
 } else {
   $('.loader').show();
+/*
   setTimeout(function(){
         $('.loader').addClass('loaded');
-    }, 1500);  
+    }, 1500); 
+*/ 
   sessionStorage.setItem('set', 'set');
 }
 });
