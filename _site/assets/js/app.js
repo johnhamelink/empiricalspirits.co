3444e144db411fd4095eba0841f455ce2257ad5e
Gareth Johns <info@garethjohnsdesign.com>
2018-05-17 13:04:46 +0100

    Loading Tweaks

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -1,5 +1,22 @@
 $(document).foundation();
 
+// 1. Loading
+// --------------------
+
+$(function() {
+if (sessionStorage.getItem('set') === 'set') {
+  $('.loader').hide();
+} else {
+  $('.loader').show();
+/*
+  setTimeout(function(){
+        $('.loader').addClass('loaded');
+    }, 1500); 
+*/ 
+  sessionStorage.setItem('set', 'set');
+}
+});
+
 // 2. Animate on Scroll
 // --------------------
 
